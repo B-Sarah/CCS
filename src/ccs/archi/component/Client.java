f3197b8617ff8efdd0f15912695517bb9ed5524e
B-Sarah <sboutahlil@gmail.com>
2017-12-04 15:07:46 +0100

    init package

---
+++ src/ccs/archi/component/Client.java
@@ -0,0 +1,11 @@
+package ccs.archi.component;
+
+import ccsM2.impl.ComponentImpl;
+
+public class Client extends ComponentImpl {
+
+	public Client() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/ccs/archi/component/Server.java
@@ -0,0 +1,11 @@
+package ccs.archi.component;
+
+import ccsM2.impl.ComponentImpl;
+
+public class Server extends ComponentImpl {
+
+	public Server() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
