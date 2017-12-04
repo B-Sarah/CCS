01cbf51f8639f1f88f9d33e809f15e9dd3274dd4
B-Sarah <sboutahlil@gmail.com>
2017-12-04 15:39:30 +0100

    finish init class

---
+++ src/ccs/archi/binding/BindRequestToClient.java
@@ -0,0 +1,11 @@
+package ccs.archi.binding;
+
+import ccsM2.impl.BindingImpl;
+
+public class BindRequestToClient extends BindingImpl {
+
+	public BindRequestToClient() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/ccs/archi/connector/Connection_Security.java
@@ -0,0 +1,11 @@
+package ccs.archi.connector;
+
+import ccsM2.impl.ConnectorImpl;
+
+public class Connection_Security extends ConnectorImpl {
+
+	public Connection_Security() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
