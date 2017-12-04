6dc1e815f7a9212925a4ee9deb41114e84a01404
B-Sarah <sboutahlil@gmail.com>
2017-12-04 18:01:52 +0100

    init database

--- src/ccs/archi/component/Database.java
+++ src/ccs/archi/component/Database.java
@@ -1,11 +1,36 @@
 package ccs.archi.component;
 
+import ccsM2.Mode;
+import ccsM2.Port;
+import ccsM2.impl.CCSFactoryImpl;
 import ccsM2.impl.ComponentImpl;
 
 public class Database extends ComponentImpl {
 
+	public enum PortName{
+		databaseToSecurityPort,
+		databaseToConnectionPort,
+		SecurityToDatabasePort,
+		ConnectionToDatabasePort
+	}
+	
 	public Database() {
 		
+		Port databaseToSecurityPort = CCSFactoryImpl.eINSTANCE.createPort();
+		Port databaseToConnectionPort = CCSFactoryImpl.eINSTANCE.createPort();
+		Port SecurityToDatabasePort = CCSFactoryImpl.eINSTANCE.createPort();
+		Port ConnectionToDatabasePort = CCSFactoryImpl.eINSTANCE.createPort();
+		
+		databaseToSecurityPort.setMode(Mode.OFFERED);
+		databaseToConnectionPort.setMode(Mode.OFFERED);
+		SecurityToDatabasePort.setMode(Mode.REQUIRED);
+		ConnectionToDatabasePort.setMode(Mode.REQUIRED);
+		
+		this.icomponentelement.add(databaseToSecurityPort);
+		this.icomponentelement.add(databaseToConnectionPort);
+
+		this.icomponentelement.add(SecurityToDatabasePort);
+		this.icomponentelement.add(ConnectionToDatabasePort);
 	}
 
 }
