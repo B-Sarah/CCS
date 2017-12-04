9f0161725dc821625af6d4b5c2d4c1d05abfcf91
Alexis DONNART <E168533N@S046V7pc02.ensinfo.sciences.univ-nantes.prive>
2017-12-04 15:40:23 +0100

    server port

--- src/ccs/archi/component/Server.java
+++ src/ccs/archi/component/Server.java
@@ -1,11 +1,24 @@
 package ccs.archi.component;
 
+import ccsM2.Mode;
+import ccsM2.Port;
 import ccsM2.impl.ComponentImpl;
 
 public class Server extends ComponentImpl {
-
+	
+	public enum PortName{
+		receive_request = 0,
+		serverRequestRedirectPort = 1
+	}
+	
 	public Server() {
-		// TODO Auto-generated constructor stub
+		Port receive_request = new Port();
+		receive_request.setMode(Mode.REQUIRED);
+		Port serverRequestRedirectPort = new Port();
+		serverRequestRedirectPort.setMode(Mode.OFFERED);
+		
+		this.icomponentelement.add(receive_request);
+		this.icomponentelement.add(serverRequestRedirectPort);
 	}
 
 }
