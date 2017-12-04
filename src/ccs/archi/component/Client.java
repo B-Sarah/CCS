4e672c32eb8b9d80bf942dd498b282375863f563
B-Sarah <sboutahlil@gmail.com>
2017-12-04 16:30:58 +0100

    Remove ignore files

--- src/ccs/archi/component/Client.java
+++ src/ccs/archi/component/Client.java
@@ -1,11 +1,30 @@
 package ccs.archi.component;
 
+import ccsM2.Mode;
+import ccsM2.Port;
+import ccsM2.impl.CCSFactoryImpl;
 import ccsM2.impl.ComponentImpl;
 
 public class Client extends ComponentImpl {
+	
+
+	public enum PortName{
+		send_request,
+		ClientRequestPort 
+	}
+	
 
 	public Client() {
-		// TODO Auto-generated constructor stub
+		//init communication port (client<->server) 
+		Port send_request = CCSFactoryImpl.eINSTANCE.createPort();
+		send_request.setMode(Mode.OFFERED);
+		
+		//init request redirect port( client->clientServer)
+		Port ClientRequestPort =  CCSFactoryImpl.eINSTANCE.createPort();
+		ClientRequestPort.setMode(Mode.OFFERED);
+		
+		this.icomponentelement.add(send_request);
+		this.icomponentelement.add(send_request);
 	}
 
 }
