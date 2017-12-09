c7256c9e64499a12a5d5b65a8a643bf11572e2e4
Lex <lx.donn@gmail.com>
2017-12-09 21:22:27 +0100

    forget RPC in last push

--- src/ccs/archi/connector/RPC.java
+++ src/ccs/archi/connector/RPC.java
@@ -1,6 +1,8 @@
 package ccs.archi.connector;
 
+import ccs.archi.component.Server.PortName;
 import ccsM2.Mode;
+import ccsM2.Port;
 import ccsM2.Role;
 import ccsM2.impl.CCSFactoryImpl;
 import ccsM2.impl.ConnectorImpl;
@@ -15,6 +17,7 @@ public class RPC extends ConnectorImpl {
 	}
 
 	public RPC() {
+		//From client to server communication
 		Role caller = CCSFactoryImpl.eINSTANCE.createRole();
 		Role called = CCSFactoryImpl.eINSTANCE.createRole();
 		Role callerResponse = CCSFactoryImpl.eINSTANCE.createRole();
@@ -29,6 +32,20 @@ public class RPC extends ConnectorImpl {
 		this.role.add(called);
 		this.role.add(callerResponse);
 		this.role.add(calledResponse);
+	}
+	
+	public Role GetRoleByName(RoleName name) {
+		switch(name) {
+		case caller:
+			return (Role)this.role.get(0);
+		case called:
+			return (Role)this.role.get(1);
+		case callerResponse:
+			return (Role)this.role.get(2);
+		case calledResponse:
+			return (Role)this.role.get(3);
+		}
+		return null;
 	}
 
 }
